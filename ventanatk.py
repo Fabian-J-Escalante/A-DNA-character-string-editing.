9f20f138ea47cb12ab7208026605971251116b7a
Emmanuel Martín Marín <123995303+EmmanuelMMartin@users.noreply.github.com>
2024-02-07 08:38:25 -0600

    Add files via upload

---
+++ ventanatk.py
@@ -0,0 +1,8 @@
+
+from tkinter import *
+from tkinter import ttk
+raiz = Tk()
+raiz.title("ACTIVIDADA 3")
+raiz.geometry("520x480")
+ttk.Label(text="HOLA MUNDO!").grid(column=0, row=0)
+raiz.mainloop()
